a4ec9692ebe66d0543dbd9a1f824b01a57dbd0d2
Lee Choon Siong <choonsiong.lee@gmail.com>
2022-11-26 16:33:19 +0800

    Auto commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 )
@@ -10,8 +11,38 @@ var views = jet.NewSet(
 	jet.NewOSFileSystemLoader("./html"),
 	jet.InDevelopmentMode())
 
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
 	err := RenderPage(w, "home.jet", nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+type WsJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType string `json:"message_type"`
+}
+
+func WsEndPoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("Client connected to websocket endpoint")
+	var resp WsJsonResponse
+	resp.Message = `<em><small>Connected to server</small></em>`
+	err = ws.WriteJSON(resp)
 	if err != nil {
 		log.Println(err)
 		return
